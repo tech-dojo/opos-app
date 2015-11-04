4ec83beb4f5616680e587c6440cce44641e3b4cd
Mahfuz <mahfuz.aftab@hotmail.com>
2015-11-04 12:57:24 +0600

    end to end testing done except report test, issue with ionic datepicker, element cannot be selected

--- tests/e2e-tests/login.tests.js
+++ tests/e2e-tests/login.tests.js
@@ -11,64 +11,67 @@ browser.driver.controlFlow().execute = function() {
     return origFn.apply(browser.driver.controlFlow(), args);
 };
 describe('Clicking on the login button ', function() {
-    var username, password, loginButton, query, OrderButton, ReportButton, showReportButton;
+    var username, password, loginButton,
+    orderList, query, OrderButton, ReportButton, showReportButton;
 
     beforeEach(function() {
         browser.get('/#/signin');
         username = element(by.model('credentials.username'));
         password = element(by.model('credentials.password'));
         query = element(by.model('query'));
+        orderCust = element(by.repeater('order in orders').row(0));
         loginButton = element(by.id('login'));
         OrderButton = element(by.id('orderBtn'));
         ReportButton = element(by.id('reportBtn'));
         showReportButton = element(by.id('showReportBtn'));
-        // startDate = element(by.binding('start'));
-        // endDate = element(by.binding('end'));
+
+         // startDate = element(by.id('startDate'));
+         // endDate = element(by.id('endDate'));
 
     });
 
-    // it('should validate the credentials for a successful login', function() {
-    //     // TODO: test successful login
+    it('should validate the credentials for a successful login', function() {
+        // TODO: test successful login
 
-    //     username.sendKeys('admin');
-    //     password.sendKeys('opos1234');
+        username.sendKeys('admin');
+        password.sendKeys('opos1234');
 
-    //     loginButton.click().then(function() {
-    //         expect(browser.getLocationAbsUrl()).toMatch('/view');
+        loginButton.click().then(function() {
+            expect(browser.getLocationAbsUrl()).toMatch('/view');
 
-    //         console.log(browser.getLocationAbsUrl());
-    //         // var dinners = element.all(by.repeater('dinner in vm.dinners'));
-    //         // expect(dinners.count()).toEqual(3);
-    //     });
-    // });
+            console.log(browser.getLocationAbsUrl());
+            // var dinners = element.all(by.repeater('dinner in vm.dinners'));
+            // expect(dinners.count()).toEqual(3);
+        });
+    });
 
-    // it('should display a popup for an unsuccessful login', function() {
-    //     // TODO: test unsuccessful login
-    //     // browser.get('/#/view');
+    it('should display a popup for an unsuccessful login', function() {
+        // TODO: test unsuccessful login
+        // browser.get('/#/view');
 
-    //     username.sendKeys('Aquib');
-    //     password.sendKeys('oposjhjkhdsfj');
-    //     loginButton.click().then(function() {
-    //     expect(browser.getLocationAbsUrl()).toMatch('/signin');
+        username.sendKeys('Aquib');
+        password.sendKeys('oposjhjkhdsfj');
+        loginButton.click().then(function() {
+        expect(browser.getLocationAbsUrl()).toMatch('/signin');
 
-    //         var popup = element(by.id('validation'));
-    //     expect(popup.isDisplayed()).toBeTruthy();
-    //     });
-    // });
+            var popup = element(by.id('validation'));
+        expect(popup.isDisplayed()).toBeTruthy();
+        });
+    });
 
 
-    // it('After successful login should be able to access orderList if orderList button is clicked', function() {
-    //     // TODO: test successful login
+    it('After successful login should be able to access orderList if orderList button is clicked', function() {
+        // TODO: test successful login
 
-    //     username.sendKeys('admin');
-    //     password.sendKeys('opos1234');
+        username.sendKeys('admin');
+        password.sendKeys('opos1234');
 
-    //     loginButton.click();
-    //     OrderButton.click().then(function(){
+        loginButton.click();
+        OrderButton.click().then(function(){
 
-    //     expect(browser.getLocationAbsUrl()).toMatch('/order');
-    //     });
-    // });
+        expect(browser.getLocationAbsUrl()).toMatch('/order');
+        });
+    });
 
 
     it('After successful access to orderList, should be able to search', function() {
@@ -81,38 +84,42 @@ describe('Clicking on the login button ', function() {
         OrderButton.click();
 
         query.sendKeys('erin');
+       
+
+       console.log(orderCust);
+        (orderCust).isDisplayed();
 
 
     });
 
-    // it('After successful login should be able to access report if report button is clicked', function() {
-    //     // TODO: test successful login
+    it('After successful login should be able to access report if report button is clicked', function() {
+        // TODO: test successful login
 
-    //     username.sendKeys('admin');
-    //     password.sendKeys('opos1234');
+        username.sendKeys('admin');
+        password.sendKeys('opos1234');
 
-    //     loginButton.click();
-    //     ReportButton.click().then(function(){
+        loginButton.click();
+        ReportButton.click().then(function(){
 
-    //     expect(browser.getLocationAbsUrl()).toMatch('/report');
-    //     });
-    // });
+        expect(browser.getLocationAbsUrl()).toMatch('/report');
+        });
+    });
 
-    // it('After accessing report if proper date range is selected, report should display', function() {
-    //     // TODO: test successful login
+    it('After accessing report if proper date range is selected, report should display', function() {
+        // TODO: test successful login
 
-    //     username.sendKeys('admin');
-    //     password.sendKeys('opos1234');
+        username.sendKeys('admin');
+        password.sendKeys('opos1234');
 
-    //     loginButton.click();
-    //     ReportButton.click();
+        loginButton.click();
+        ReportButton.click();
 
-    //     startDate.sendKeys('Oct-03-2015');
-    //     endDate.sendKeys('Oct-24-2015');
+        startDate.sendKeys('Oct-03-2015');
+        endDate.sendKeys('Oct-24-2015');
 
-    //    showReportButton.click();
+       showReportButton.click();
 
-    // });
+    });
 
 
 });
